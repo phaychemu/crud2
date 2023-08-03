11889630248a12d0eb68969ac411d410a4d171d4
Queenvinemuthoni <queenvinemuthoni@gmail.com>
2023-08-03 03:40:45 -0400

    initial commit

---
+++ myapp/urls.py
@@ -0,0 +1,12 @@
+
+from django.contrib import admin
+from django.urls import path
+from myapp import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.register,name='register'),
+    path('login/', views.login,name='login'),
+    path('home/', views.home,name='home'),
+
+]
